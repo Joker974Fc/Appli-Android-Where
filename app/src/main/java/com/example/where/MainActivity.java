1e4bb7ff9603f80b98f95dbd8ccb3a63c3a793f9
florian <florian.clain17@gmail.com>
2020-03-31 15:02:39 +0400

    31/03/2020 menu test 2.0 Implémentation de la page d'acceuille pour l'application (Peut-être modifier avant la fin du projet).

--- app/src/main/java/com/example/where/MainActivity.java
+++ app/src/main/java/com/example/where/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.where;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.where.fragments.DasboardFragment;
+import com.example.where.fragments.FavoriteFragment;
+import com.example.where.fragments.HomeFragment;
+//import com.bottomnavigationview.fragments.NotificationFragment;
+//import com.bottomnavigationview.fragments.SmsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_dash:
+                            openFragment(DasboardFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_notif:
+                            openFragment(FavoriteFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
