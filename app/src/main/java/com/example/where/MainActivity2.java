f65729f8ae04b185f2f85ecac7596b6771ef954e
florian <florian.clain17@gmail.com>
2020-04-03 10:23:16 +0400

    03/04/2020: Implémentation de la liste Produits.

--- app/src/main/java/com/example/where/MainActivity2.java
+++ app/src/main/java/com/example/where/MainActivity2.java
@@ -5,8 +5,10 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-
-import com.example.where.fragments.Boutique;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,8 @@ public class MainActivity2 extends AppCompatActivity {
     private RecyclerView mRecycle;
     private List<Boutique> boutiques;
     private BoutiqueAdapter mAdapt;
+    private List<Boutique> fav;
+    private List<Produit> prod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +32,7 @@ public class MainActivity2 extends AppCompatActivity {
 
         boutiques.add(new Boutique("boutique1", "tomate,laitue,oignon","St Denis"));
         boutiques.add(new Boutique("boutique2", "tomate, brède, haricot","St Denis"));
-        boutiques.add(new Boutique("boutique3", "pomme de terre, cerise","St Suzane"));
+        boutiques.add(new Boutique("boutique3", "pomme de terre, cerise","La Possession"));
         boutiques.add(new Boutique("boutique4", "tomate,laitue,oignon","St Denis"));
         boutiques.add(new Boutique("boutique5",  "tomate,laitue,oignon","St Denis"));
         boutiques.add(new Boutique("boutique6", "tomate,laitue,oignon","St Denis"));
@@ -39,5 +43,45 @@ public class MainActivity2 extends AppCompatActivity {
 
         mRecycle.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
         mRecycle.setAdapter(mAdapt);
+
+        prod = new ArrayList<>();
+
+        prod.add(new Produit("Tomate",true,"T"));
+        prod.add(new Produit("Laitue",true,"L"));
+        prod.add(new Produit("Carote",true,"C"));
+        prod.add(new Produit("Oignon",true,"Oi"));
+        prod.add(new Produit("Navet",true,"N"));
+
+
+
+
+
+
     }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        fav = new ArrayList<>();
+        switch(item.getItemId()) {
+            case R.id.act_fav:
+
+            case R.id.action_delete:
+                //adapter.remove(adapter.getItem(menuInfo.position));
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+
+
+
+
 }
