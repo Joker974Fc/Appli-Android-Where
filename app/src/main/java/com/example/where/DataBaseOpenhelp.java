3982992f444ffa6e84528eab084700ae1b8ecf42
florian <florian.clain17@gmail.com>
2020-04-21 15:25:34 +0400

    21/04/2020 ajout d'une base de données externe contenant les tables boutique, produit et favori

---
+++ app/src/main/java/com/example/where/DataBaseOpenhelp.java
@@ -0,0 +1,48 @@
+package com.example.where;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseOpenhelp extends SQLiteAssetHelper {
+    public static final String DATABASE_NAME = "boutique.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public DataBaseOpenhelp(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+
+    }
+
+    //get all boutique
+    public List<Boutique> getBoutique(){
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        String[] sqlSelect = {"ID","NOM","PRODUIT","VILLE"};
+        String tableName="lboutique";
+
+        qb.setTables(tableName);
+        Cursor c = qb.query(db,sqlSelect,null,null,null,null,null);
+        List<Boutique> result = new ArrayList<>();
+        if (c.moveToFirst()){
+            do {
+                Boutique boutique = new Boutique();
+                boutique.setId(c.getInt(c.getColumnIndex("ID")));
+                boutique.setName(c.getString(c.getColumnIndex("NOM")));
+                boutique.setArticle(c.getString(c.getColumnIndex("PRODUIT")));
+                boutique.setCity(c.getString(c.getColumnIndex("VILLE")));
+                result.add(boutique);
+            }while (c.moveToNext());
+        }
+        return result;
+
+    }
+
+
+}
